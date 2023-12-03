6c44fe7bf8d8c5c380e66f31206bd3cbbcef9c3b
raidengreene <142539260+raidengreene@users.noreply.github.com>
2023-12-03 00:30:57 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,27 @@ $( document ).ready(function() {
   
   $("#bdPush").click(function bdPush() {
     $("#bddoor").attr("src","img/bdOpen.jpg");
+    $("#bddoor").attr("alt","open door");
     document.getElementById("bdAnswer").innerHTML = "That is correct! How much did you hesitate?";
   });
   
-  $("#bdPull").click(function bdPush() {
-    document.getElementById("bdAnswer").innerHTML = "Wrong! ";
+  $("#bdPull").click(function bdPull() {
+    $("#bddoor").attr("src","img/bdClose.jpg");
+    $("#bddoor").attr("alt","closed door");
+    document.getElementById("bdAnswer").innerHTML = "Wrong! Now you look like an idiot!";
   });
   
+  $("#gdPush").click(function gdPush() {
+    $("#gddoor").attr("src","img/gdOpen.jpg");
+    $("#gddoor").attr("alt","open door");
+    document.getElementById("gdAnswer").innerHTML = "That is correct! See how easy this is?";
+  });
   
-  
-  
+  $("#gdPull").click(function gdPull() {
+    $("#gddoor").attr("src","img/gdclose.jpg");
+    $("#gddoor").attr("alt","closed door");
+    document.getElementById("gdAnswer").innerHTML = "Why would you press that? It literally tells you waht to do.";
+  });
 
 });
 
